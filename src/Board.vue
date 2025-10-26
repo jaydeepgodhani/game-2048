bafdbf4cbec2d45787e141f41a3c940d174e18d8
Jaydeep Godhani <jaydeep@Hnvk.local>
2025-10-26 18:25:40 +0530

    svg added

--- src/Board.vue
+++ src/Board.vue
@@ -49,8 +49,26 @@ onBeforeUnmount(() => {
       <Cell v-for="cell in row" :key="cell.id" :number="cell.id" :value="cell.value" />
     </div>
     <div class="mt-8">
-      <button @click="handleClick" class="py-2 px-8 m-4 border-2 rounded-full text-xl">
-        Reset
+      <button
+        @click="handleClick"
+        class="py-2 px-8 m-4 border-2 rounded-full text-xl flex items-center"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+        &nbsp;
+        <span> Reset </span>
       </button>
     </div>
     <div class="mt-8 text-xl flex flex-col items-center justify-center">
@@ -119,7 +137,7 @@ onBeforeUnmount(() => {
 
       <div>
         <span class="bg-gray-100 border-slate-300 border-2 rounded-lg px-2">ctrl + z</span>
-        to undo
+        to undo once
       </div>
     </div>
   </div>
