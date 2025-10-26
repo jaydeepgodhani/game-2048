f6681fe522988a4dc5733330530f58da2832eb6d
Jaydeep Godhani <jaydeep@Hnvk.local>
2025-10-26 19:22:14 +0530

    transition added

--- src/Board.vue
+++ src/Board.vue
@@ -45,43 +45,40 @@ onBeforeUnmount(() => {
 
 <template>
   <div class="flex items-center flex-col">
-    <h1 class="text-6xl my-16">2048</h1>
+    <h1 class="text-6xl my-16 flex">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="size-14"
+      >
+        <path
+          d="M11.25 5.337c0-.355-.186-.676-.401-.959a1.647 1.647 0 0 1-.349-1.003c0-1.036 1.007-1.875 2.25-1.875S15 2.34 15 3.375c0 .369-.128.713-.349 1.003-.215.283-.401.604-.401.959 0 .332.278.598.61.578 1.91-.114 3.79-.342 5.632-.676a.75.75 0 0 1 .878.645 49.17 49.17 0 0 1 .376 5.452.657.657 0 0 1-.66.664c-.354 0-.675-.186-.958-.401a1.647 1.647 0 0 0-1.003-.349c-1.035 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401.31 0 .557.262.534.571a48.774 48.774 0 0 1-.595 4.845.75.75 0 0 1-.61.61c-1.82.317-3.673.533-5.555.642a.58.58 0 0 1-.611-.581c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.035-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959a.641.641 0 0 1-.658.643 49.118 49.118 0 0 1-4.708-.36.75.75 0 0 1-.645-.878c.293-1.614.504-3.257.629-4.924A.53.53 0 0 0 5.337 15c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.036 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.369 0 .713.128 1.003.349.283.215.604.401.959.401a.656.656 0 0 0 .659-.663 47.703 47.703 0 0 0-.31-4.82.75.75 0 0 1 .83-.832c1.343.155 2.703.254 4.077.294a.64.64 0 0 0 .657-.642Z"
+        />
+      </svg>
+      &nbsp; 2048
+    </h1>
     <div v-for="row in grid" :key="row[0].id" class="flex flex-row justify-center">
       <Cell v-for="cell in row" :key="cell.id" :number="cell.id" :value="cell.value" />
     </div>
     <div class="mt-8">
       <button
         @click="handleClick"
-        class="py-2 px-8 m-4 border-2 rounded-full text-xl flex items-center"
+        class="py-2 px-8 m-4 border-2 rounded-full text-xl flex items-center cursor-pointer bg-gray-100 transition duration-400 hover:drop-shadow-lg/30"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-          />
-        </svg>
-        &nbsp;
-        <span> Reset </span>
+        Reset
       </button>
     </div>
     <div class="mt-8 text-xl flex flex-col items-center justify-center">
       <div class="flex py-2">
         Use Arrow keys &nbsp;
-        <Svg value="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+        <Svg value="M15.75 19.5 8.25 12l7.5-7.5" />
         &nbsp;
-        <Svg value="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+        <Svg value="m8.25 4.5 7.5 7.5-7.5 7.5" />
         &nbsp;
-        <Svg value="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18" />
+        <Svg value="m4.5 15.75 7.5-7.5 7.5 7.5" />
         &nbsp;
-        <Svg value="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+        <Svg value="m19.5 8.25-7.5 7.5-7.5-7.5" />
       </div>
 
       <div>
